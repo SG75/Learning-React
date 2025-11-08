b3e4caeb458b243053cb3a8fdb8123d2a1239e35
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2025-11-08 22:24:07 +0530

    episode 1

--- script.js
+++ script.js
@@ -10,7 +10,44 @@
 
 //React.createElement() function signature is createElement(type, props, ...children).
 
-const heading = React.createElement("h1", {}, "Om Ganeshaya Namaha");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//ReactElement(Object) => HTML(Browser Understands)
 
-root.render(heading)
+
+
+const heading = React.createElement("h1", {id: "heading", parents: "shiva and parvathi"}, ["Om Ganeshaya Namaha",[(
+         React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{}, "i am H1 tag from child 1"),
+        React.createElement("h2",{}, "i am H2 tag from child 1"),
+    ])), React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{}, "i am H1 tag from child 2"),
+        React.createElement("h2",{}, "i am H2 tag from child 2"),
+    ])
+    ]]);
+
+    /// reactdom will take control of the div with id root and will replace the content inside that div with the react element we created
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const guru = ReactDOM.createRoot(document.getElementById("root")); 
+// updated variable name it need not be root
+console.log(heading);
+console.log(root);
+
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},[(
+         React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{}, "i am H1 tag from child 1"),
+        React.createElement("h2",{}, "i am H2 tag from child 1"),
+    ])), React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{}, "i am H1 tag from child 2"),
+        React.createElement("h2",{}, "i am H2 tag from child 2"),
+    ])
+    ]
+   
+);
+
+//
+
+guru.render(heading);
+// root.render(parent);
